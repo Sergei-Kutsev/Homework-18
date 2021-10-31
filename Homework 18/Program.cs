c41ae73c332d4603a01ae3505aa633114d6866c0
Сергей Куцев <sergeikutsev@yandex.ru>
2021-11-01 00:45:01 +0300

    Добавьте файлы проекта.

---
+++ Homework 18/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_18
+{
+    class Bracket
+    {
+
+        string[] brackets = { "(", "{", "[", "]", "}", "]" };
+        public IEnumerator<string> GetEnumerator()
+        {
+            return new IEnumenatorBrackets(brackets);
+        }
+    }
+    class IEnumenatorBrackets : IEnumerator<string>
+    {
+        string[] brackets;
+        int position = -1;
+        Stack<string> Bracketlist = new Stack<string>();
+        public IEnumenatorBrackets(string[] brackets)
+        {
+            this.brackets = brackets;
+        }
+        public string Current
+        {
+            get
+            {
+                return brackets[position];
+            }
+        }
+        object IEnumerator.Current => throw new NotImplementedException();
+        void RoundPush()
+        {
+            if (Current == "(")
+            { Bracketlist.Push(")"); }
+            else
+            {
+                try 
+                { 
+                    string roundBracket = Bracketlist.Pop();
+                    if (roundBracket != ")") 
+                    { 
+                        Console.WriteLine("Скобки НЕ в правильном порядке"); 
+                        Console.ReadKey(); 
+                        Environment.Exit(0);
+                    } 
+                }
+                catch (InvalidOperationException)
+                { 
+                    Console.WriteLine("Скобки НЕ в правильном порядке");
+                    Console.ReadKey(); 
+                    Environment.Exit(0);
+                }
+            }
+        } // Обрабатывет круглые скобки, если они есть.
+        void SquarePush()
+        {
+            if (Current == "[")
+            { Bracketlist.Push("]"); }
+            else
+            {
+                try 
+                {
+                    string roundBracket = Bracketlist.Pop(); 
+                    if (roundBracket != "]") 
+                    {
+                        Console.WriteLine("Скобки НЕ в правильном порядке");
+                        Console.ReadKey(); 
+                        Environment.Exit(0);
+                    }
+                }
+                catch (InvalidOperationException)
+                { 
+                    Console.WriteLine("Скобки НЕ в правильном порядке"); 
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                }
+            }
+        }// Обрабатывет квадратные скобки, если они есть.
+        void CurlyPush()
+        {
+            if (Current == "{")
+            { Bracketlist.Push("}"); }
+            else
+            {
+                try
+                {
+                    string roundBracket = Bracketlist.Pop();
+                    if (roundBracket != "}")
+                    {
+                        Console.WriteLine("Скобки НЕ в правильном порядке");
+                        Console.ReadKey();
+                        Environment.Exit(0);
+                    }
+                }
+                catch (InvalidOperationException)
+                {
+                    Console.WriteLine("Скобки НЕ в правильном порядке"); 
+                    Console.ReadKey(); 
+                    Environment.Exit(0); ;
+                }
+            }
+        }// Обрабатывет фигурные скобки, если они есть.
+        public void Dispose() { }
+        public bool MoveNext()
+        {
+            if (position < brackets.Length - 1)
+            {
+                position++;
+                if (Current == "(" || Current == ")") { RoundPush(); }
+                if (Current == "[" || Current == "]") { SquarePush(); }
+                if (Current == "{" || Current == "}") { CurlyPush(); }
+                return true;
+            }
+            else
+                foreach (string a in Bracketlist) 
+                { 
+                    Console.WriteLine("Скобки НЕ в правильном порядке"); 
+                    Console.ReadKey(); 
+                    Environment.Exit(0);
+                }//Если зашли, значит коллекция Stack не пуста
+            return false;
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+    }
+    class Public
+    {
+        static void Main(string[] args)
+        {
+            
+            Bracket list = new Bracket();
+            foreach (string a in list)
+            {
+                Console.WriteLine(a);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Скобки расставлены в ПРАВИЛЬНОМ порядке");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_18
+{
+    /*Указание: задача решается однократным проходом по символам заданной строки слева направо; для каждой открывающей скобки в строке в стек 
+     помещается соответствующая закрывающая, каждая закрывающая скобка в строке должна соответствовать скобке из вершины стека 
+     (при этом скобка с вершины стека снимается); в конце прохода стек должен быть пуст.*/
+
+    class Bracket
+    {
+
+        string[] brackets = { ")" };
+
+        public IEnumerator GetEnumerator()
+        {
+            return new IEnumenatorBrackets(brackets);
+        }
+
+
+    }
+    class IEnumenatorBrackets : IEnumerator
+    {
+        string[] brackets;
+        int position = -1;
+
+        public IEnumenatorBrackets(string[] brackets)
+        {
+            this.brackets = brackets;
+        }
+        public object Current
+        {
+            get
+            { return brackets[position]; }
+        }
+        public bool MoveNext()
+        {
+            if (position < brackets.Length - 1)
+            {
+                position++;
+                return true;
+            }
+            else
+                return false;
+        }
+        public void Reset()
+        {
+            position = -1;
+        }
+    }
+    class Public
+    {
+        static void Main(string[] args)
+        {
+            string roundBracket = null;
+            string squareBracket = null;
+            string curlyBracket = null;
+            string queueElement = null;
+            Queue<string> brackets = new Queue<string>();
+            Console.Write("Введите строку состоящую из скобок для проверки их последовательности: ");
+            Bracket list = new Bracket();
+
+            foreach (string a in list)
+            {
+                Console.WriteLine(a);
+                if (a == "(")
+                {
+                    roundBracket = a;
+                    brackets.Enqueue(roundBracket);
+                }
+                if (a == ")")
+                {
+                    queueElement = brackets.Dequeue();
+                    if (queueElement == null)
+                    { Console.WriteLine("Скобки НЕ в правильном порядке"); Console.ReadKey(); return; }
+                }
+                roundBracket = null;
+                if (a == "[") 
+                {
+                    squareBracket = a; 
+                }
+                squareBracket = null;
+                if (a == "}") 
+                {
+                    curlyBracket = a; 
+                }
+                curlyBracket = null;
+
+            }
+
+            foreach (string a in brackets)
+            { Console.WriteLine("Скобки расставлены НЕ в правильном порядке"); Console.ReadKey(); return;}
+
+            Console.WriteLine("Скобки расставлены в правильном порядке");
+            Console.ReadKey();
+        }
+    }
+}
